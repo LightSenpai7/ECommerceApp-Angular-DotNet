91bad81588f2157dbb0a51b56b16b3da83148e46
Yunus Emre Koçak <95947411+LightSenpai7@users.noreply.github.com>
2025-05-13 19:25:37 +0300

    Separation of Production and Development Environments and Appsettings Configuration

--- Presentation/ECommerceApp.API/Program.cs
+++ Presentation/ECommerceApp.API/Program.cs
@@ -3,14 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();          
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
