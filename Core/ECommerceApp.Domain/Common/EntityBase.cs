e305a2f65efcc8f9b6deea25a738be052f884861
Yunus Emre Koçak <95947411+LightSenpai7@users.noreply.github.com>
2025-05-14 17:38:39 +0300

    Creation of entities

---
+++ Core/ECommerceApp.Domain/Common/EntityBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Domain.Common
+{
+    public class EntityBase: IEntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+        public EntityBase()
+        {
+            CreatedDate = DateTime.UtcNow;
+            UpdatedDate = DateTime.UtcNow;
+            IsDeleted = false;
+            IsActive = true;
+        }
+    }
+}
